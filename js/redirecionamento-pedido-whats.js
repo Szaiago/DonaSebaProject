516fcfbd5ecf7cf2c2dbc4046f3529abfe1aba1a
Iago Souza <iago.snd.souza@gmail.com>
2024-06-30 13:55:40 -0300

    atualizações

--- js/redirecionamento-pedido-whats.js
+++ js/redirecionamento-pedido-whats.js
@@ -7,7 +7,7 @@ function redirectToWhatsApp() {
     const nomeProduto = document.getElementById('nome-pedido').textContent;
     const precoProduto = document.getElementById('price').textContent;
     const mensagem = `Olá, gostaria de saber mais sobre o ${nomeProduto} que está com preço de ${precoProduto}`;
-    const numeroTelefone = '4998087342';
+    const numeroTelefone = '4791587771';
     const url = `https://wa.me/55${numeroTelefone}?text=${encodeURIComponent(mensagem)}`;
     window.location.href = url;
 }
