c3bdb15e2324a51610cc54e1a6a7eac16352179c
Iago Souza <iago.snd.souza@gmail.com>
2024-07-01 09:54:35 -0300

    Revert "atualizações"
    This reverts commit 47e0a3e9f989f9b69e392e21ca9c907fcef354e3.

--- js/redirecionamento-pedido-whats.js
+++ js/redirecionamento-pedido-whats.js
@@ -10,7 +10,6 @@ function enviarWhatsApp() {
     const rua = document.getElementById('Rua-span').textContent.trim();
     const errorContainer = document.getElementById('error-container');
 
-    // Validação dos campos
     if (!nomeCliente || !cep || !dataEntrega || !cidade || !bairro || !rua || !nomePedido || !price) {
         errorContainer.style.display = 'block';
         document.getElementById('Nome-Cliente-span').textContent = !nomeCliente ? 'Nome do Cliente é obrigatório.' : '';
@@ -25,7 +24,6 @@ function enviarWhatsApp() {
         return;
     }
 
-    // Construção da mensagem
     const mensagem = `Olá, gostaria de fazer um pedido:\n\n` +
         `Nome do Cliente: ${nomeCliente}\n` +
         `CEP: ${cep}\n` +
@@ -37,14 +35,10 @@ function enviarWhatsApp() {
         `Nome do Pedido: ${nomePedido}\n` +
         `Preço: ${price}`;
 
-    // Abre a janela do WhatsApp apenas se todos os dados estiverem corretos
     const numeroWhatsApp = '47991587771';
     const urlWhatsApp = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensagem)}`;
-    
-    // Verifica se há erros de validação antes de abrir o WhatsApp
-    if (errorContainer.style.display === 'none') {
-        window.open(urlWhatsApp, '_blank');
-    }
+
+    window.open(urlWhatsApp, '_blank');
 }
 
 document.addEventListener('DOMContentLoaded', function() {
