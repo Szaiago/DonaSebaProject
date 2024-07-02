98cfba7fd1f7f7156191e56a0f9b05cc828c3eb9
Iago Souza <iago.snd.souza@gmail.com>
2024-07-02 18:32:59 -0300

    atualizações

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,28 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-
     const dropdown = document.querySelector('.dropdown');
+    const hamburger = document.querySelector('.hamburger');
     const dropdownContent = document.querySelector('.dropdown-content');
-    
 
-    dropdown.addEventListener('click', function(event) {
+    hamburger.addEventListener('click', function(event) {
         dropdownContent.classList.toggle('show');
+        event.stopPropagation(); // Impede que o clique se propague para o window
     });
-
-    const dropdownAjuda = document.querySelector('.dropdown-ajuda');
-    const dropdownContentAjuda = document.querySelector('.dropdown-content-ajuda');
-    
-
-    dropdownAjuda.addEventListener('click', function(event) {
-        dropdownContentAjuda.classList.toggle('show');
-    });
-
 
     window.addEventListener('click', function(event) {
         if (!dropdown.contains(event.target)) {
             dropdownContent.classList.remove('show');
         }
-        if (!dropdownAjuda.contains(event.target)) {
-            dropdownContentAjuda.classList.remove('show');
-        }
+    });
+
+    dropdownContent.addEventListener('click', function(event) {
+        event.stopPropagation(); // Impede que o clique se propague para o window
     });
 });
